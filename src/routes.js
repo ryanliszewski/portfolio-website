0957f2e596131f30164d3402eb98adc95892ca4c
Ryanliszewski <ryanliszewski@gmail.com>
2018-02-07 16:27:54 -0800

    first commit

---
+++ src/routes.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import App from './App';
+//import NotFound from './NotFound';
+
+const Routes = (props) => {
+  return (
+    <Router {...props}>
+      <Switch>
+      <Route exact path="/" component={App} />
+
+      </Switch>
+    </Router>
+  )
+}
+
+export default Routes; 
